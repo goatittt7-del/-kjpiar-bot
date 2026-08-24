c97ef88d4b38758cc89fefc38c62a3de1522fea2
goatittt7-del <goatittt7@gmail.com>
2026-08-24 16:57:16 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -46,12 +46,16 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         text,
         reply_markup=keyboard
     )
-
+async def promotion(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "📢 Что вы хотите рекламировать?",
+    )
 
 def main():
     app = Application.builder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.Regex("^📢 Продвижение$"), promotion))
 
     app.run_polling()
 
